ade65510c0efba09a3b681028ee8eb472db29475
anlunjiang <anlunjiang@outlook.com>
2020-02-27 00:53:51 +0000

    make window

--- src/com/noodles/game/Window.java
+++ src/com/noodles/game/Window.java
@@ -4,4 +4,14 @@ import javax.swing.JFrame;
 // JFrame makes windows in Java
 
 public class Window extends JFrame {
+
+    public Window() {
+        setTitle("MyFirstJavaWindow");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //allows to exit the window
+        setContentPane(new GamePanel(1280, 720));
+        pack(); // compacts window
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
 }
