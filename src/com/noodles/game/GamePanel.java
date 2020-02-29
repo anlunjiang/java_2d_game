dc1988d22124c4de1ea6887d32ee4b2ae40a6b06
Anlun <anlunjiang@outlook.com>
2020-02-29 01:37:30 +0000

    initiate gameloop and fps_tkrate (#2)

--- src/com/noodles/game/GamePanel.java
+++ src/com/noodles/game/GamePanel.java
@@ -1,18 +1,122 @@
 package com.noodles.game;
 
 import javax.swing.JPanel;
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable { // creates a thread
 
     public static int width;
     public static int height;
 
-    public GamePanel(int width, int height) {
+    private Thread thread;
+    private BufferedImage img;
+    private Graphics2D g;
+    private boolean running = false;
 
+    public GamePanel(int width, int height) {
+        this.width = width; // "this" replaced with the instance object name (GamePanel.width = width)
+        this.height  = height;
         setPreferredSize(new Dimension(width, height));
         setFocusable(true);
         requestFocus(); // allows jpanel to have input when jframe is made
     }
 
+    public void addNotify() {
+        super.addNotify(); // inputs from KBM
+
+        if(thread == null) {
+            thread = new Thread(this, "GameThread");
+            thread.start();
+        }
+    }
+
+    public void init() {
+        running = true;
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB); // will be drawn onto screen
+        g = (Graphics2D) img.getGraphics(); // can draw on the buffered image
+    }
+
+    public void run() {
+        init();
+        // fixed time game loop
+        final double GAME_HERTZ = 60.0;
+        final double TBU =  1000000000 / GAME_HERTZ; // time before update nanoseconds
+
+        final int MUBR = 5; // Max Updates before render
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+
+        final double TARGET_FPS = 30.0;
+        final double TTBR = 1000000000 / TARGET_FPS; // Total Time Before Render
+
+        int frameCount = 0;
+        int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+        int oldFrameCount = 0;
+
+        while(running) {
+            double now = System.nanoTime();
+            int updateCount = 0;
+
+            while(((now - lastUpdateTime) > TBU) && (updateCount < MUBR)) {
+                update();
+                input();
+                lastUpdateTime += TBU;
+                updateCount++;
+            }
+
+            if(now - lastUpdateTime > TBU) {
+                lastUpdateTime = now - TBU;
+            }
+
+            render();
+            draw();
+            lastRenderTime = now;
+            frameCount++;
+
+            int thisSecond = (int) (lastUpdateTime / 1000000000);
+            if(thisSecond > lastSecondTime) {
+                if (frameCount != oldFrameCount) {
+                    System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+                    oldFrameCount = frameCount;
+                }
+                frameCount = 0;
+                lastSecondTime = thisSecond;
+            }
+
+            while(now - lastRenderTime < TTBR && now - lastUpdateTime < TBU) {
+                Thread.yield();
+                try {
+                    Thread.sleep(1);
+                } catch(Exception e) {
+                    System.out.println("ERROR: Yielding thread");
+                }
+                now = System.nanoTime();
+            }
+        }
+    }
+
+    private int x = 0;
+
+    public void update() {
+
+    }
+
+    public void render() {
+        if(g != null) {
+            g.setColor(new Color(66, 134,244));
+            g.fillRect(0, 0, width, height); // background
+        }
+    }
+
+    public void input() {
+
+    }
+
+    public void draw() {
+        Graphics g2 = (Graphics) this.getGraphics(); // instantiate another graphics obj
+        g2.drawImage(img, 0, 0, width, height, null); // draws onto screen
+        g2.dispose(); // need to remove after
+    }
 }
